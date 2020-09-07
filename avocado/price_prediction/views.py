ef6faa4083fd885f9dde38c456178aca59e7f589
noyb <noyb@localhost.localdomain>
2020-09-07 15:19:51 -0400

    Add Json Parser ParserClass to view

--- avocado/price_prediction/views.py
+++ avocado/price_prediction/views.py
@@ -1,21 +1,7 @@
-from django.http import HttpRequest, HttpResponse
-from django.template import loader
-import io
-
-from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
-from .serializers import AvocadoPredictionSerializer
 from PricePredictionModel import model
-
-import pandas as pd
-from datetime import datetime
-import io
-
-from PricePredictionModel import model
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpResponse
 from django.template import loader
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, parser_classes
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 
@@ -23,24 +9,6 @@ from .serializers import AvocadoPredictionSerializer
 
 
 # Create your views here.
-
-
-@api_view(["GET", "POST"])
-def get_price_prediction(request: HttpRequest):
-    """ Gets data about an avocado and returns a price prediction for it """
-    # TODO:
-    # 1. Data validation on request
-    # 2. send to predict method
-    # 3. return
-    request_body = io.BytesIO(request.body)
-    data = JSONParser().parse(request_body)
-    serializer = AvocadoPredictionSerializer(data=data)
-
-    if serializer.is_valid():
-        return HttpResponse(JSONRenderer().render(predict_price(serializer.validated_data)))
-
-    else:
-        return HttpResponse(content="Error invalid request", status=400)
 
 
 def predict_price(avocado_order):
@@ -52,3 +20,16 @@ def predict_price(avocado_order):
 def get_prediction_page(request):
     template = loader.get_template("price_prediction/form-dumb.html")
     return HttpResponse(template.render())
+
+
+@api_view(["GET", "POST"])
+@parser_classes([JSONParser])
+def get_price_prediction(request, format=None):
+    """ Gets data about an avocado and returns a price prediction for it """
+    serializer = AvocadoPredictionSerializer(data=request.data)
+
+    if serializer.is_valid():
+        return HttpResponse(JSONRenderer().render(predict_price(serializer.validated_data)))
+
+    else:
+        return HttpResponse(content="Error invalid request", status=400)
